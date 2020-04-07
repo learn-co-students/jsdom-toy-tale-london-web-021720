8f330980787f8dd5a18605eaef14ee10191ac2c3
nickzaf95 <nicholaszafiriou@gmail.com>
2020-04-07 15:09:53 +0200

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 let addToy = false;
 
+fetch("http://localhost:3000/toys") 
+.then(response => response.json())
+.then(renderToys => renderEachToy(renderToys))
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyForm = document.querySelector(".container");
@@ -11,5 +16,88 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       toyForm.style.display = "none";
     }
+  });
+});
+
+
+const postToy = toy =>
+  fetch("http://localhost:3000/toys", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(toy)
+  }).then(response => {
+    return response.json();
+  });
+
+const patchToy = toy =>
+  fetch(`http://localhost:3000/toys/${toy.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(toy)
+  }).then(response => {
+    return response.json();
+});
+
+
+function renderEachToy(toys) {
+  toys.forEach(toy => {
+    const toyCollection = document.querySelector("#toy-collection")
+    const toyCard = document.createElement("div")
+    const toyName = document.createElement("h2")
+    const toyImage = document.createElement("img")
+    const toyLikes = document.createElement("p")
+    const toyLikeBtn = document.createElement("button")
+
+    toyCard.className = "card"
+    toyName.innerText = toy.name 
+    toyImage.src = toy.image
+    toyImage.className = "toy-avatar"
+    toyLikes.innerText = `${toy.likes} Likes`
+    toyLikeBtn.className = "like-btn"
+    toyLikeBtn.innerText = "Like <3"
+
+    toyLikeBtn.addEventListener("click", () => {
+      console.log("like this toy", toy);
+  
+      toy.likes++;
+      toyLikeBtn.disabled = true;
+  
+      patchToy(toy).then(updatedToy => {
+        toy.likes = updatedToy.likes;
+  
+        toyLikes.innerText = `Likes: ${toy.likes}`;
+        toyLikeBtn.disabled = false;
+      });
+    });
+
+    toyCard.appendChild(toyName)
+    toyCard.appendChild(toyImage)
+    toyCard.appendChild(toyLikes)
+    toyCard.appendChild(toyLikeBtn)
+
+    toyCollection.appendChild(toyCard)
+  });
+}
+
+const newToyForm = document.querySelector("form");
+
+console.log(newToyForm);
+newToyForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const newToy = {
+    name: newToyForm.name.value,
+    image: newToyForm.image.value,
+    likes: 0
+  };
+
+  postToy(newToy).then(newToyFromServer => {
+    renderToy(newToyFromServer);
   });
 });
